7e23781b1370ee2f89b77815863117d64b024bb8
BruGeth <g15bru03@hotmail.com>
2026-01-14 01:00:32 -0500

    feat(dto): add CategoryRequest and CategoryResponse classes with mapping methods in CategoryMapper

---
+++ src/main/java/com/coquito/backend/dto/category/CategoryRequest.java
@@ -0,0 +1,22 @@
+package com.coquito.backend.dto.category;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryRequest {
+    
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
+    private String name;
+    
+    @Size(max = 255, message = "Description cannot exceed 255 characters")
+    private String description;
+}
